e72e2f66da3fa75533da9d80e8d54921a578cdec
Seyeon321 <dhtpdus@kaist.ac.kr>
2024-07-07 17:25:49 +0900

    frontend connection

--- App.js
+++ App.js
@@ -9,12 +9,17 @@ app.use(express.json());
 
 app.use('/users', usersRouter);
 
-app.get('/', (req, res)=>
-{
+//엔드포인트 설정
+app.post("/auth/kakao/callback", (req, res) => {
+    const user = req.body; //요청 본문에서 데이터 읽기
+    console.log('Recieved user data:', user); // 데이터 처리 로직 추가
+    res.status(201).send("User data received and processed.");
+});
+
+app.get('/', (req, res)=>{
     res.send(`<h2>welcome to server</h2>`);
 });
 
-app.listen(port, ()=>
-{
+app.listen(port, ()=>{
    console.log(`SERVER 실행됨 ${port}`); 
 });
